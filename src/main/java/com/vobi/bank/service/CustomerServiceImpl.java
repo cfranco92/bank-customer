5903f4a5a25748c4ffc6fe6cd9f08c85273be5a8
Cristian Franco <cfrancobedoya@gmail.com>
2022-02-12 09:58:35 -0500

    Implement CustomerService

--- src/main/java/com/vobi/bank/service/CustomerServiceImpl.java
+++ src/main/java/com/vobi/bank/service/CustomerServiceImpl.java
@@ -3,23 +3,45 @@ package com.vobi.bank.service;
 import java.util.List;
 import java.util.Optional;
 
-import com.vobi.bank.domain.Customer;
+import javax.validation.Validator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.vobi.bank.domain.Customer;
+import com.vobi.bank.repository.CustomerRepository;
+
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Autowired
+	Validator validator;
+
 	@Override
+	@Transactional(readOnly = true)
 	public List<Customer> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return customerRepository.findAll();
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Optional<Customer> findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		return customerRepository.findById(id);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
+	public Long count() {
+		return customerRepository.count();
+	}
+
+	@Override
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public Customer save(Customer entity) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
@@ -34,25 +56,19 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public void delete(Customer entity) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void validate(Customer entity) throws Exception {
 		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public Long count() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
