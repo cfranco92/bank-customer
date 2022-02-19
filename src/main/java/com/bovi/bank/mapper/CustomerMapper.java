f114269836485625325d2d74a22759b7fda8fe97
Cristian Franco <cfrancobedoya@gmail.com>
2022-02-18 20:00:17 -0500

    Add CustomerToDTO

---
+++ src/main/java/com/bovi/bank/mapper/CustomerMapper.java
@@ -0,0 +1,13 @@
+package com.bovi.bank.mapper;
+
+import org.mapstruct.Mapper;
+
+import com.vobi.bank.domain.Customer;
+import com.vobi.bank.dto.CustomerDTO;
+
+@Mapper
+public interface CustomerMapper {
+
+	public CustomerDTO customerToCustomerDTO(Customer customer);
+	
+}
